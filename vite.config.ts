9a43691b7d478c3f6b633b4d91b0baffec8fe67a
brocadeyu <yjb1725594062@gmail.com>
2025-01-13 15:32:55 +0800

    perf: load env

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  server: {
-    port: 5177,
-    host: '0.0.0.0',
-    // /api/login -> http://localhost:12000/login
-    proxy: {
-      '/api': {
-        target: 'http://localhost:12000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    server: {
+      port: 5177,
+      host: '0.0.0.0',
+      // /api/login -> {env.VITE_APP_PROXY}/login
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_PROXY,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+      open: true,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  }
 })
