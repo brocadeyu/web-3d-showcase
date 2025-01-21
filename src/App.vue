e9264a1ef3d52e9fe09075cca6bc5357e695d5bb
brocadeyu <yjb1725594062@gmail.com>
2025-01-21 09:19:31 +0800

    feat: elplus autoImport

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <el-button>测试</el-button>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/about">UI Components</RouterLink>
       </nav>
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,26 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [vue(), vueJsx(), vueDevTools()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        dts: './src/types/auto-imports.d.ts',
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dts: './src/types/components.d.ts',
+      }),
+    ],
     server: {
       port: 5177,
       host: '0.0.0.0',
